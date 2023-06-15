12be7383552c7b4d8fb45fe939c244c1b7558098
Adrian <acastillo459@gmail.com>
2023-06-15 15:10:25 -0500

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -33,7 +33,11 @@ app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://admin:admin@cluster0.lc5uunb.mongodb.net/local_library?retryWrites=true&w=majority";
+// Set up mongoose connection
+const dev_db_url =
+  "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 
 main().catch((err) => console.log(err));
 async function main() {
