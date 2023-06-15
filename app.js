8f56cac71c7eb88a080038dbb05654ed1f585dca
Adrian <acastillo459@gmail.com>
2023-06-15 15:49:08 -0500

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 // Set up mongoose connection
 const dev_db_url =
-  "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+  "mongodb+srv://admin:admin@cluster0.lc5uunb.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = dev_db_url;
 
 
 main().catch((err) => console.log(err));
