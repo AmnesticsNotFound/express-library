95ce614a089f916234ae9c2737f4012304e55280
Adrian <acastillo459@gmail.com>
2023-06-16 12:37:26 -0500

    update

--- app.js
+++ app.js
@@ -34,7 +34,12 @@ app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 // Set up mongoose connection
-const mongoDB = "mongodb+srv://admin:admin@cluster0.lc5uunb.mongodb.net/local_library?retryWrites=true&w=majority";
+// Set up mongoose connection
+const dev_db_url =
+  "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+
 
 
 main().catch((err) => console.log(err));
